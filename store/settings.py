bf28f6707fe85f91ad1797573129de86fedc2e5f
nikemelyanov <emelyanovtime@gmail.com>
2024-01-23 16:54:45 +0300

    Initial commit

--- store/settings.py
+++ store/settings.py
@@ -20,7 +20,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-sprk4sclg43ak@px_e&)!a)q+4796u)en8=3zr)jwq_!qbcvz0'
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
